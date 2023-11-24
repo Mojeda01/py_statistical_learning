9c6aa725fa46e656b434d1361f5e807d1269bd23
Mojeda01 <marco.oj01@gmail.com>
2023-11-24 16:47:12 +0100

    updated diagnostics.py

--- linear_regression_diagnostics/diagnostics.py
+++ linear_regression_diagnostics/diagnostics.py
@@ -16,3 +16,30 @@ class LinearRegDiagnostic():
         if isinstance(results, statsmodels.regression.linear_model.RegressionResultsWrapper) is False:
             raise TypeError("Results must be instance of statsmodels.regression.linear_model.RegressionResultsWrapper object")
         
+        self.results = maybe_unwrap_results(results)
+
+        self.y_tue = self.results.model.endog
+        self.y_predict = self.results.fittedvalues
+        self.xvar = self.results.model.exog
+        self.xvar_names = self.results.model.exog_names
+
+        self.residual = np.array(self.results.resid)
+        influence = self.results.get_influence()
+        self.residual_norm = influence.resid_studentized_internal
+        self.leverage = influence.hat_matrix_diag
+        self.cooks_distance = influence.cooks_distance[0]
+        self.nparams = len(self.results.params)
+        self.nresids = len(self.residual_norm)
+    
+    def residual_plot(self, ax=None):
+        
+        if ax is None:
+            fig, ax = plt.subplots()
+        
+        sns.residplot(
+            x=self.y_predict,
+            y=self.residual,
+            lowess=True,
+            scatter_kws={"alpha":0.5},
+            line_kws={"Color":"red", "lw":1, "alpha":0.8},
+            ax=ax)
